9ed5cb015ea32788bd79b5441b5cea10cc33996d
Mladen Brankovic <root@brankovic.dev>
2022-01-19 22:08:26 +0100

    chore: migrate nix environment to a flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A work-in-progress, general purpose Discord bot written in Rust.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, flake-compat }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+      in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = [ rust-bin.stable.latest.default ];
+        };
+      }
+    );
+}
