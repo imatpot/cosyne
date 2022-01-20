2d08ec4c6f469b63d6d2ad554775cd97511b2154
Mladen Brankovic <root@brankovic.dev>
2022-01-20 19:24:54 +0100

    feat: add latency to ping

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,12 +1,23 @@
 // Plagiarized from https://github.com/kangalioo/poise/blob/master/src/samples.rs
 
+use chrono::Local;
 use poise::{command, say_reply};
 
-use crate::types::poise::{Context, Error};
+use crate::{
+    types::poise::{Context, Error},
+    util::logger::Logger,
+};
 
 /// Check whether the bot is responsive.
 #[command(rename = "ping", slash_command, prefix_command)]
 pub async fn exec(context: Context<'_>) -> Result<(), Error> {
-    say_reply(context, "Pong!").await?;
+    let latency = Local::now()
+        .signed_duration_since(context.created_at())
+        .num_milliseconds();
+
+    say_reply(context, format!("Pong! It took me `{latency}ms` to react")).await?;
+
+    Logger::debug(&format!("Latency: {latency}ms"));
+
     Ok(())
 }
