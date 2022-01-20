c2b711e7a9cb079a6dc793d8c0622c4d86ed4e4d
Mladen Brankovic <root@brankovic.dev>
2022-01-20 16:24:15 +0100

    chore: use fenix overlay in nix environment

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -12,16 +16,23 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils, fenix, flake-compat }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ fenix.overlay ];
         pkgs = import nixpkgs { inherit system overlays; };
 
       in
       with pkgs; {
         devShell = mkShell {
-          buildInputs = [ rust-bin.stable.latest.default ];
+          buildInputs = [
+            (pkgs.fenix.stable.withComponents [
+              "cargo"
+              "rustc"
+              "rustfmt"
+              "rust-src"
+            ])
+          ];
         };
       }
     );
